3a39f77aa495b6c77720384678b98bd5e40dde98
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-09-16 21:58:30 -0700

    Started working of creating an Application by reading the existing containers.

--- topanga/application.py
+++ topanga/application.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from docker import Client
+from .container import Container
 
 
 class Application(object):
@@ -16,8 +18,9 @@ class Application(object):
     client = None
     containers = {}
     filename = None
+    label_key = 'topanga.app'
 
-    def __init__(self, name, client, containers=None, filename=None):
+    def __init__(self, name, client=None, containers=None, filename=None):
         """
         Initialize the Application
 
@@ -30,6 +33,16 @@ class Application(object):
         self.containers = containers
         self.filename = filename
 
+        if containers:
+            for container in containers:
+                self.containers[container.name] = container
+        elif filename:
+            instance = self.create_from_filename(filename)
+            self.containers = instance.containers
+        elif client:
+            instance = self.create_from_client(name, client)
+            self.containers = instance.containers
+
     def add_container(self, container):
         """
         Add a container to application
@@ -37,14 +50,51 @@ class Application(object):
         """
         self.containers[container.name] = container
 
-    def _create_from_client(self, client):
-        # TODO
-        pass
+    @classmethod
+    def create_from_client(cls, name, client):
+        """
+        Read all containers running in docker with either this name prefix,
+        or this name tag.
 
-    def _create_from_yaml(self, filename):
-        # TODO
-        pass
+        :param name: string unique name
+        :param client: `Client`
+        :return: `Application`
+        """
 
-    def _create_from_containers(self, containers):
+        assert isinstance(client, Client)
+
+        cs = {}
+
+        def has_application_tag(c):
+            """ Is container marked with this application name? """
+            return c.get('Labels', {}).get(cls.label_key) and \
+                   c['Labels'][cls.label_key] == name
+
+        def get_name(c):
+            """ Retrieve container name from dict. """
+            return c['Names'][0].lstrip('/')
+
+        # Go through all containers and look at
+        for c in client.containers(all=True):
+            if has_application_tag(c):
+                # TODO:
+                # we should be able to create an instance of Container class
+                # from docker-py's Container dict.
+                cs[get_name(c)] = Container.create_from_dict(c)
+
+        if cs:
+            return Application(containers=cs)
+        else:
+            raise Exception(
+                'There are no containers for application {0}'.format(name))
+
+    @classmethod
+    def create_from_filename(cls, filename):
+        """
+        Read all containers from the provided YAML file.
+
+        :param name: string unique name
+        :return: `Application`
+        """
         # TODO
         pass
